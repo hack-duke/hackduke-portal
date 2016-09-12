6aee077f51be412b8a1bba1f1aadd91b8e418ea6
David Yang <dy46@duke.edu>
2016-09-12 02:14:23 -0400

    added animation to navMenu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,20 +1,47 @@
 import React from 'react'
 import classes from './NavMenu.scss'
 import { IndexLink } from 'react-router'
+import { pushRotate as Menu } from 'react-burger-menu'
 
 class NavMenu extends React.Component {
+  constructor () {
+    super()
+    this.state = {enabled: false, width: '0px', left: '0px'}
+  }
+
+  handleClick () {
+    const width = !this.state.enabled ? '200px' : '0px'
+    const left = !this.state.enabled ? '200px' : '0px'
+    this.setState({width: width, left: left, enabled: !this.state.enabled})
+  }
 
   static propTypes = {
-    logout: React.PropTypes.func.isRequired
+    logout: React.PropTypes.func.isRequired,
+    click: React.PropTypes.func.isRequired
   }
 
   render () {
     return (
-      <div className={classes.navMenu}>
-        <IndexLink className={classes.linkElement} to={'/dashboard'}> Dashboard </IndexLink>
-        <IndexLink className={classes.linkElement} to={'/application'}> Application </IndexLink>
-        <div onClick={this.props.logout}>
-          <IndexLink className={classes.linkElement} to={'/login'}> Log out </IndexLink>
+      <div>
+        <div>
+          <button onClick={this.handleClick.bind(this)}
+            className={classes.menuButton}
+            style={{'left': `${this.state.left}`}}>
+            <img src='hackduke-logo.png' />
+          </button>
+          <div className={classes.navMenu}
+            style={{'width': `${this.state.width}`}}>
+            <img src='hackduke-logo.png' alt='Hackduke Logo' />
+            <div className={classes.menuItem}>
+              <IndexLink className={classes.linkElement} to={'/dashboard'}> DASHBOARD </IndexLink>
+            </div>
+            <div className={classes.menuItem}>
+              <IndexLink className={classes.linkElement} to={'/application'}> APPLICATION </IndexLink>
+            </div>
+            <div className={classes.menuItem}>
+              <IndexLink onClick={this.props.logout} className={classes.linkElement} to={'/login'}> LOGOUT </IndexLink>
+            </div>
+          </div>
         </div>
       </div>
     )
