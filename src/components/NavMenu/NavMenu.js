9366cbf43163918f368cdc6139e6f4ec953ea9e3
David Yang <dy46@duke.edu>
2016-09-15 16:28:17 -0400

    Nav menu (#14)
    * added animation to navMenu
    
    * fixed animation of navmenu
    
    * trying whatwg/fetch
    
    * TODO: fix Safari Header error (Andy's fix didn't work
    
    * now works with Safari
    
    * added a constant for 768
    
    * fixed lint error
    
    * added some tests

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -4,17 +4,60 @@ import { IndexLink } from 'react-router'
 
 class NavMenu extends React.Component {
 
+  constructor () {
+    super()
+    this.handleResize = this.handleResize.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {enabled: false, menuLeft: '0px', buttonLeft: '0px'}
+  }
+
+  handleResize (e) {
+    // Added this constant to explain the 768
+    const tabletWidth = 768
+    const menuLeft = window.innerWidth < tabletWidth ? '-250px' : '0px'
+    const buttonLeft = window.innerWidth < tabletWidth ? '0px' : '250px'
+    this.setState({menuLeft: menuLeft, buttonLeft: buttonLeft})
+  }
+
+  componentDidMount () {
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  handleClick () {
+    const menuLeft = !this.state.enabled ? '0px' : '-250px'
+    const buttonLeft = !this.state.enabled ? '250px' : '0px'
+    this.setState({menuLeft: menuLeft, buttonLeft: buttonLeft, enabled: !this.state.enabled})
+  }
+
   static propTypes = {
     logout: React.PropTypes.func.isRequired
   }
 
   render () {
     return (
-      <div className={classes.navMenu}>
-        <IndexLink className={classes.linkElement} to={'/dashboard'}> Dashboard </IndexLink>
-        <IndexLink className={classes.linkElement} to={'/application'}> Application </IndexLink>
-        <div onClick={this.props.logout}>
-          <IndexLink className={classes.linkElement} to={'/login'}> Log out </IndexLink>
+      <div>
+        <button onClick={this.handleClick}
+          className={classes.menuButton}
+          style={{'left': `${this.state.buttonLeft}`}}>
+          <img src='hamburger-icon.png' />
+        </button>
+        <div className={classes.navMenu}
+          style={{'left': `${this.state.menuLeft}`}}>
+          <img src='hackduke-logo.png' alt='Hackduke Logo' />
+          <div className={classes.menuItem}>
+            <IndexLink className={classes.linkElement} to={'/dashboard'}>DASHBOARD</IndexLink>
+          </div>
+          <div className={classes.menuItem}>
+            <IndexLink className={classes.linkElement} to={'/application'}>APPLICATION</IndexLink>
+          </div>
+          <div className={classes.menuItem}>
+            <IndexLink onClick={this.props.logout} className={classes.linkElement} to={'/login'}>LOGOUT</IndexLink>
+          </div>
         </div>
       </div>
     )
