3f36b4f1277b5055290f52c0a23a08956aa085bd
Briguy52 <Briguy52@gmail.com>
2016-09-18 14:19:52 -0400

    added test for status (still needs more robust checks)

--- tests/components/Dashboard.spec.js
+++ tests/components/Dashboard.spec.js
@@ -12,28 +12,28 @@ describe('(Component) Dashboard', () => {
       participant: {
         'person': {'first_name': 'George', 'email': 'george.smith@gmail.com'},
         'role': {
-          "event_id": 13,
-          "team_id": null,
-          "status": "accepted",
-          "graduation_year": 2018,
-          "over_eighteen": 1,
-          "attending": null,
-          "major": "Computer Science",
-          "school": "North Carolina State University",
-          "dietary_restrictions": [
-            "None"
+          'event_id': 13,
+          'team_id': null,
+          'status': 'accepted',
+          'graduation_year': 2018,
+          'over_eighteen': 1,
+          'attending': null,
+          'major': 'Computer Science',
+          'school': 'North Carolina State University',
+          'dietary_restrictions': [
+            'None'
           ],
-          "website": null,
-          "resume": null,
-          "github": "www.github.com",
-          "travel": null,
-          "portfolio": null,
-          "skills": [],
-          "custom": [
-            "Q: Why do you want to attend Ideate?",
-            "asdasd",
-            "Q: Tell us about your design experience.",
-            "asdasd"
+          'website': null,
+          'resume': null,
+          'github': 'www.github.com',
+          'travel': null,
+          'portfolio': null,
+          'skills': [],
+          'custom': [
+            'Q: Why do you want to attend Ideate?',
+            'asdasd',
+            'Q: Tell us about your design experience.',
+            'asdasd'
           ]
         }
       }
@@ -42,8 +42,13 @@ describe('(Component) Dashboard', () => {
   })
 
 
-  it("renders a header", function () {
-    expect(_wrapper.find('.' + classes.header)).to.have.length(1)
+  it('renders a header', function () {
+    expect(_wrapper.find('.' + classes.header).filterWhere(p => p.text() === 'DASHBOARD')).to.have.length(1)
+  })
+
+  it('renders the correct status', function() {
+    expect(_wrapper.find('.' + classes.status).filterWhere(p => p.text() === 'ACCEPTED')).to.have.length(1)
+    // TODO: pass in status from props and check against props in this class
   })
 
 })
