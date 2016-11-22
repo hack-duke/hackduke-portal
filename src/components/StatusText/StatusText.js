57c8531f3ebb2d60e7932a4f75e00feaf2b54a46
Andy Wang <ownz.andy@gmail.com>
2016-11-22 11:13:26 -0500

    removing link

--- src/components/StatusText/StatusText.js
+++ src/components/StatusText/StatusText.js
@@ -33,10 +33,7 @@ function generateMessage (status, email) {
 function generateConfirmedJSX (email) {
   return (
     <div>
-      {'Whoo! We can\'t wait to see you! If you\'re flying or driving from out-of-state, don\'t forget to '}
-      <a href={'https://hackduke.typeform.com/to/w4ZA3F?route_update_participant=xxxxx&email=' + email}
-        className={classes.link}>{'upload'}</a>
-      {' receipts. We will reimburse up to $200 for flights and $50 for gas, but we can\'t reimburse people from schools with buses.'}
+      {'Whoo! We can\'t wait to see you! If you\'re flying or driving from out-of-state, we will reimburse up to $200 for flights and $50 for gas, but we can\'t reimburse people from schools with buses.'}
     </div>
   )
 }
